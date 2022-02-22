1ca98529670ad9887b404f9b75d6ff6202a89fb1
SashaShark14 <93825914+SashaShark14@users.noreply.github.com>
2022-02-22 23:25:13 +0300

    task in processing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   
+  const pairs = {
+    '(': ')', 
+    '[': ']', 
+    '{': '}', 
+    '|': '|', 
+    '1': '2', 
+    '3': '4', 
+    '5': '6', 
+    '7': '7', 
+    '8': '8', 
+  }
+
+   let stack = []
+   let flatArr = bracketsConfig.flat()
+let openBrackets = []
+for(let k = 0; k < flatArr.length; k++) {
+    if(k % 2 === 0) {
+        openBrackets.push(flatArr[k])        
+    }    
 }
+
+const closing = Object.values(pairs);  
+        
+  for (let char of str) {
+    if (pairs[char]) {
+      stack.push(char);
+    } else if (closing.includes(char) && char !==pairs[stack.pop()]) {
+      return false;
+    }
+  }
+  return !stack.length;
+
+// for(let i = 0; i < str.length; i++) {
+//      let currentSymbol = str[i]
+
+//      if(openBrackets.includes(currentSymbol)) {
+//             stack.push(currentSymbol)
+//           } else {
+//             if(stack.length === 0) {
+//               return false
+//             }
+//             let lastElement = stack[stack.length - 1]
+      
+//             if(pairs[currentSymbol === lastElement]) {
+//               stack.pop()
+//             } else {
+//               return false
+//             }
+//           }
+//         }
+//        return stack.length === 0
+      
+
+// }
+
+
+}
+
+
